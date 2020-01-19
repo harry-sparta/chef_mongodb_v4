be216f0b2c0c5b60dfdee96eabd8f8ada9679391
harry-sparta <ka.hei.harry@gmail.com>
2020-01-19 20:28:28 +0000

    working mongodb but not passing the IP 0.0.0.0 integration test even though it is on 0.0.0.0 on mongo.conf

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,51 @@
 # Recipe:: default
 #
 # Copyright:: 2020, The Authors, All Rights Reserved.
+
+# --------- BASH COMMANDS FOR MONGODB INSTALL
+execute 'mongod_apt_key'do
+  command 'wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add -'
+  action :run
+end
+
+execute 'mongod_update_sourcelist' do
+  command "echo 'deb [ arch=amd64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.2 multiverse' | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list"
+  action :run
+end
+
+execute 'apt-update' do
+  command 'sudo apt-get update -y'
+  action :run
+end
+
+execute 'mongodb_install' do
+  command 'sudo apt-get install -y mongodb-org'
+  action :run
+end
+
+# execute 'mongod_provision_script' do
+#   command 'sudo systemctl restart mongod'
+#   action :run
+# end
+#
+# execute 'mongod_provision_script' do
+#   command 'sudo systemctl enable mongod'
+#   action :run
+# end
+
+service 'mongod' do
+  action [:start, :enable]
+end
+
+# resource template
+template '/etc/mongod.conf' do
+  source 'mongod.conf.erb'
+  variables port: node['mongod']['port'], bindIp: node['mongod']['bindIp']
+  notifies :restart, 'service[mongod]'
+end
+
+
+template '/lib/systemd/system/mongod.service' do
+  source 'mongod.service.erb'
+  notifies :restart, 'service[mongod]'
+end
--- spec/unit/recipes/default_spec.rb
+++ spec/unit/recipes/default_spec.rb
@@ -11,19 +11,30 @@ describe 'mongo_v4::default' do
     # for a complete list of available platforms and versions see:
     # https://github.com/chefspec/fauxhai/blob/master/PLATFORMS.md
     platform 'ubuntu', '18.04'
+  end
 
     it 'converges successfully' do
       expect { chef_run }.to_not raise_error
     end
-  end
 
-  context 'When all attributes are default, on CentOS 7' do
-    # for a complete list of available platforms and versions see:
-    # https://github.com/chefspec/fauxhai/blob/master/PLATFORMS.md
-    platform 'centos', '7'
+  # context 'When all attributes are default, on CentOS 7' do
+  #   # for a complete list of available platforms and versions see:
+  #   # https://github.com/chefspec/fauxhai/blob/master/PLATFORMS.md
+  #   platform 'centos', '7'
 
     it 'converges successfully' do
       expect { chef_run }.to_not raise_error
     end
-  end
+
+    it 'should install mongodb' do
+      expect(chef_run).to run_execute 'mongodb_install'
+    end
+
+    it 'should create a mongod.conf template in /etc' do
+      expect(chef_run).to create_template('/etc/mongod.conf').with_variables(port: 27017, bindIp: '0.0.0.0')
+    end
+
+    it 'runs apt-get update' do
+      expect(chef_run).to update_apt_update 'update_sources'
+    end
 end
